5df9e300fef8e0a842ea98facbfb1e04d0e9eb79
Phamminhduy <phamminhduy97@gmail.com>
2017-07-22 20:32:17 +0700

    add code

--- app/src/main/java/com/example/phamm/demotoast/MainActivity.java
+++ app/src/main/java/com/example/phamm/demotoast/MainActivity.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btntoast;
+    TextView txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btntoast = (Button) findViewById(R.id.btnToast);
+        txtView  = (TextView) findViewById(R.id.txtView);
+
 
         btntoast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Click Click !", Toast.LENGTH_SHORT).show();
+                txtView.setText("Android Studio");
             }
         });
     }
